47319b84a899e6e97eddd4d3e61b499a0be322a0
Eren GÜLTEKİN <erik_nami@hotmail.com>
2022-01-12 01:50:28 +0300

    DogsAnim

---
+++ CyberCat/Assets/Scripts/Event/DogAnimation.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class DogAnimation : MonoBehaviour
+{
+    EnemyController enemyController;
+    Animator _animator;
+    TwoBoneIKConstraint DogLeftHandChain;
+    MultiAimConstraint DogRightHandAim;
+    void Start()
+    {
+        enemyController = this.gameObject.transform.GetChild(2).GetComponent<EnemyController>();
+        _animator = GetComponent<Animator>();
+        DogLeftHandChain = this.gameObject.transform.GetChild(3).gameObject.transform.GetChild(1).GetComponent<TwoBoneIKConstraint>();
+        DogLeftHandChain.weight = 0;
+        DogRightHandAim = this.gameObject.transform.GetChild(3).gameObject.transform.GetChild(0).gameObject.transform.GetChild(0).GetComponent<MultiAimConstraint>();
+        DogRightHandAim.weight = 0;
+    }
+
+    
+    void Update()
+    {
+        DogDetectedPlayer();
+    }
+    void DogDetectedPlayer()
+    {
+        if (enemyController._detected)
+        {
+            _animator.SetBool("DogDetectPlayer", true);
+            DogLeftHandChain.weight = 1;
+            DogRightHandAim.weight = 1;
+        }
+        else if (!enemyController._detected)
+        {
+            _animator.SetBool("DogDetectPlayer", false);
+            DogLeftHandChain.weight = 0;
+            DogRightHandAim.weight = 0;
+        }
+    }
+
+}
